3cb4c7281abfe3cd33da1cdc8bc2aeed2147624e
Tasha <xtasherx@gmail.com>
2020-08-08 15:11:32 -0500

    fixed bugs: user typing NaN, clearing pw each time button is pressed

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-// TO DO:
-
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
 let password = "";
+let pass = "";
 let optionString = "";
+const passwordText = document.querySelector("#password");
+
 // object to contain types of chars available
 const passOptions = {
   upper: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
@@ -21,29 +22,30 @@ function checkType(type, string) {
 
 generateBtn.addEventListener("click", () => {
   let userLen = "";
+
   // Loop to ensure the user picks the correct password length
   do {
     userLen = prompt(
       "Please choose a length for your password between 8 and 128 characters."
     );
-    console.log(userLen);
-  } while (+userLen < 8 || +userLen > 128);
+  } while (+userLen < 8 || +userLen > 128 || isNaN(+userLen));
 
   // Store the user char choices
   const userCaps = confirm("Would you like to use uppercase letters?");
   const userLow = confirm("Would you like to use lowercase letters?");
   const userNums = confirm("Would you like to use numbers?");
-  const userSpecial = confirm("Would you like to use special charachters?");
+  const userSpecial = confirm("Would you like to use special characters?");
 
   // Generate the password
   function generatePassword() {
     // Verify user character choices and pw length--add types of chars to string if chosen
+    optionString = "";
     checkType(userCaps, passOptions.upper);
     checkType(userLow, passOptions.lower);
     checkType(userNums, passOptions.numeric);
     checkType(userSpecial, passOptions.special);
     let passLength = +userLen;
-
+    let password = "";
     // Produce random char from optionString at each iteration and add it to the password variable to create new string.
     for (let i = 0; i < passLength; i++) {
       const randomMath = Math.floor(Math.random() * optionString.length);
@@ -59,9 +61,12 @@ generateBtn.addEventListener("click", () => {
 
   // Write password to the #password input
   function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-    passwordText.value = password;
+    pass = generatePassword();
+    if (pass === undefined) {
+      passwordText.value = "";
+    } else {
+      passwordText.value = pass;
+    }
   }
 
   writePassword();
